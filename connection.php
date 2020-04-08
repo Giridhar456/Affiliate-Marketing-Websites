b2f1897e2364085750a509a21403435d24841b98
GiridharKrishna <35100733+Giridhar456@users.noreply.github.com>
2020-04-08 16:02:47 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $connect=mysqli_connect("sql304.epizy.com","epiz_23196514","appece456","epiz_23196514_mpes");
+    $connect=mysqli_connect("","","","");
     if ($connect->connect_error) {
         die("Connection failed: " . $conn->connect_error);
         echo "Fail To Connect Database..!";    
